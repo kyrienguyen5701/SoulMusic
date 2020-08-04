79b93385ba080214c63fccbac4c85477614ac41b
deodeocute <deodeo2k@gmail.com>
2020-08-04 23:54:33 +0700

    gan xong man search

--- App.js
+++ App.js
@@ -6,12 +6,61 @@ import Icon from 'react-native-vector-icons/FontAwesome5';
 import Player from 'screens/Player';
 import SearchBar from 'screens/Search/components/SearchBar';
 import DefaultSearch from 'screens/Search/DefaultSearch';
-import miniPlayer from 'screens/Player/miniPlayer'
+import miniPlayer from 'screens/Player/miniPlayer';
 import PlayerFullScreen from 'screens/Player';
-import { enableScreens } from 'react-native-screens';
-
+import {enableScreens} from 'react-native-screens';
+import {View, Text, TouchableOpacity} from 'react-native';
 enableScreens();
 
+// function MyTabBar({state, descriptors, navigation}) {
+//   return (
+//     <View style={{flexDirection: 'row'}}>
+//       {state.routes.map((route, index) => {
+//         const {options} = descriptors[route.key];
+//         const label =
+//           options.tabBarLabel !== undefined
+//             ? options.tabBarLabel
+//             : options.title !== undefined
+//             ? options.title
+//             : route.name;
+
+//         const isFocused = state.index === index;
+
+//         const onPress = () => {
+//           const event = navigation.emit({
+//             type: 'tabPress',
+//             target: route.key,
+//           });
+
+//           if (!isFocused && !event.defaultPrevented) {
+//             navigation.navigate(route.name);
+//           }
+//         };
+
+//         const onLongPress = () => {
+//           navigation.emit({
+//             type: 'tabLongPress',
+//             target: route.key,
+//           });
+//         };
+
+//         return (
+//           <TouchableOpacity
+//             accessibilityRole="button"
+//             accessibilityStates={isFocused ? ['selected'] : []}
+//             accessibilityLabel={options.tabBarAccessibilityLabel}
+//             testID={options.tabBarTestID}
+//             onPress={onPress}
+//             onLongPress={onLongPress}
+//             style={{flex: 1, height: 100}}
+//             key={index}>
+//             <Text style={{color: isFocused ? '#673ab7' : '#222'}}>{label}</Text>
+//           </TouchableOpacity>
+//         );
+//       })}
+//     </View>
+//   );
+// }
 
 const Tab = createBottomTabNavigator();
 
@@ -36,16 +85,14 @@ export default function App() {
         tabBarOptions={{
           activeTintColor: 'red',
           inactiveTintColor: 'gray',
-          style:{
-            backgroundColor:"'#0C08C3"
-          }
+          tabStyle: {
+            backgroundColor: '#0C08C3',
+            height:90
+          },
         }}
-
-        
-        initialRouteName={'Playlists'}
-        >
-        <Tab.Screen name="miniPlayer" component={miniPlayer}/>
-        <Tab.Screen name="DefaultSearch" component={DefaultSearch}/>
+        initialRouteName={'DefaultSearch'}>
+        <Tab.Screen name="miniPlayer" component={miniPlayer} />
+        <Tab.Screen name="DefaultSearch" component={DefaultSearch} />
         <Tab.Screen name="Playlists" component={Playlists} />
         <Tab.Screen name="Player" component={Player} />
         <Tab.Screen name="More" component={SearchBar} />
--- src/screens/Search/DefaultSearch.tsx
+++ src/screens/Search/DefaultSearch.tsx
@@ -6,13 +6,64 @@ import {
   StyleSheet,
   Dimensions,
   TouchableOpacity,
+  ImageBackground,
 } from 'react-native';
-import {TextInput} from 'react-native-gesture-handler';
+import {TextInput, FlatList} from 'react-native-gesture-handler';
 import LinearGradient from 'react-native-linear-gradient';
 import {Song} from 'components/Song';
 import {useNavigation} from '@react-navigation/native';
+import {createConfigItem} from '@babel/core';
 
 export const width_screen = Dimensions.get('window').width;
+
+const data = [
+  {
+    id: 'jgZkrA8E5do',
+    title: 'TOULIVER x BINZ - -BIGCITYBOI- (Official Music Video)',
+    channel: 'Binz Da Poet',
+    url:
+      'http://42.112.17.21:8077/TOULIVER%20x%20BINZ%20-%20-BIGCITYBOI-%20(Official%20Music%20Video).mp4',
+    genre: 'V-Pop',
+    top: 1,
+  },
+  {
+    id: 'K-a8s8OLBSE',
+    title: 'Taylor Swift - cardigan (Official Music Video)',
+    channel: 'Taylor Swift',
+    url:
+      'http://42.112.17.21:8077/Taylor%20Swift%20-%20cardigan%20(Official%20Music%20Video).mp4',
+    genre: 'Pop',
+    top: 2,
+  },
+  {
+    id: 'Ujb-gvqsoi0',
+    title: "Red Velvet - IRENE & SEULGI 'Monster' MV",
+    channel: 'SMTOWN',
+    url:
+      "http://42.112.17.21:8077/Red%20Velvet%20-%20IRENE%20&%20SEULGI%20'Monster'%20MV.mp4",
+    genre: 'K-Pop',
+    top: 3,
+  },
+  {
+    id: 'LZN4I3K8SC0',
+    title:
+      'Cứ Chill Thôi - Chillies (Official Music Video) ft Suni Hạ Linh & Rhymastic',
+    channel: 'Chillies',
+    url:
+      'http://42.112.17.21:8077/C%E1%BB%A9_Chill_Th%C3%B4i_Chillies_Official_Music_Video_ft_Suni_H%E1%BA%A1_Linh_&_Rhymastic.mp4',
+    genre: 'V-Pop',
+    top: 4,
+  },
+  {
+    id: 'HlN2BXNJzxA',
+    title: "CHUNG HA 청하 'Gotta Go (벌써 12시)' Official MV",
+    channel: 'MNH Entertainment',
+    url:
+      "http://42.112.17.21:8077/CHUNG_HA_%EC%B2%AD%ED%95%98_'Gotta_Go_%EB%B2%8C%EC%8D%A8_12%EC%8B%9C'_Official_MV.mp4",
+    genre: 'K-Pop',
+    top: 5,
+  },
+];
 
 const DefaultSearch = (props: Song) => {
   const navigation = useNavigation();
@@ -43,7 +94,6 @@ const DefaultSearch = (props: Song) => {
             style={styles.input}
             placeholder={'Search music'}
             placeholderTextColor="#AC1C1C"
-            
           />
         </View>
 
@@ -55,34 +105,45 @@ const DefaultSearch = (props: Song) => {
           <Text style={styles.trend}>Trending in Vietnam</Text>
         </View>
 
-        <View style={styles.container}>
-          <View>
-            <Text style={styles.top}>#1</Text>
-            <TouchableOpacity
-              onPress={() => navigation.navigate('PlayerFullScreen', props)}>
-              <Image
-                style={styles.minImage}
-                source={{
-                  uri: `https://i.ytimg.com/vi/${props.id}/hqdefault.jpg`,
-                }}
-              />
+        <ImageBackground source={require('assets/Rectangle99.png')} style={styles.container}>
+         <View style={styles.container}>
+            <FlatList
+              keyExtractor={(item, index) => index}
+              data={data}
+              renderItem={({item}) => (
+                <TouchableOpacity style={{height: 70 , borderRadius: 10,}}>
+                  <Text key={item.top} style={styles.top}>
+                    #{item.top}
+                  </Text>
 
-              <View style={styles.component}>
-                <Text style={styles.title}>{props.title}</Text>
-                <Text style={styles.channel}>Sơn Tùng M-TP Official</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
+                  <Image
+                    key={item.id}
+                    source={{
+                      uri: `https://i.ytimg.com/vi/${item.id}/hqdefault.jpg`,
+                    }}
+                    style={styles.minImage}
+                  />
+                  <View style={styles.component}>
+                    <Text key={item.title} style={styles.title}>
+                      {item.title}
+                    </Text>
 
-          <Image
-            style={{
-              width: '100%',
-              borderRadius: 10,
-              opacity: 0.3,
-            }}
-            source={require('../../assets/lili.jpg')}
-          />
-        </View>
+                    <Text key={item.channel} style={styles.channel}>
+                      {item.channel}
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              )}
+            />
+            </View>
+          <Text style={{color:'#ffffff' ,
+                         textAlign:'center' ,
+                         fontSize:12,
+                         padding:13,
+                         opacity:0.7
+
+              }}>See all</Text>
+        </ImageBackground>
       </View>
     </LinearGradient>
   );
@@ -103,7 +164,6 @@ const styles = StyleSheet.create({
     height: 45,
     marginLeft: ((width_screen - 30) % 2) + 45,
     marginTop: 15,
-    position: 'absolute',
     borderRadius: 5,
   },
   trend: {
@@ -114,6 +174,8 @@ const styles = StyleSheet.create({
     marginLeft: 15,
     marginRight: 15,
     marginTop: 20,
+    borderRadius:10
+  
   },
   top: {
     fontSize: 12,
@@ -123,7 +185,7 @@ const styles = StyleSheet.create({
     color: '#FFFFFF',
   },
   component: {
-    width: 192,
+    width: '60%',
     height: 76,
     marginLeft: 135,
     marginTop: 15,
