5cc916bca60702d62728070166a44fec6412c8ae
deodeocute <deodeo2k@gmail.com>
2020-08-13 00:34:47 +0700

    hom nay la 13/8 lan 2

--- src/screens/Playlists/DefaultPlaylists.tsx
+++ src/screens/Playlists/DefaultPlaylists.tsx
@@ -1,13 +1,12 @@
-import React, {useState} from "react";
+import React, {useState} from 'react';
 import {FlatList, Text, TouchableOpacity, View, Dimensions} from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 import {SongData} from 'components/Song';
-import Recent from "screens/Playlists/Recent";
+import Recent from 'screens/Playlists/Recent';
 import LinearGradient from 'react-native-linear-gradient';
 
 export const width_screen = Dimensions.get('window').width;
 
-<<<<<<< HEAD
 const BasicGenres = ['V-Pop', 'K-Pop', 'Pop', 'Blue'];
 
 const DefaultPlaylists = () => {
@@ -59,90 +58,5 @@ const DefaultPlaylists = () => {
     </LinearGradient>
   );
 };
-=======
-// @ts-ignore
-const PlaylistButton = (props) => {
-    const navigation = useNavigation();
-
-    return (
-        <TouchableOpacity
-            style={{
-                width: "40%",
-                height: 24,
-                marginHorizontal: 10,
-                marginVertical: 10,
-                backgroundColor: 'azure',
-                borderRadius: 10
-            }}
-            onPress={() => navigation.navigate('Playlist', props.title)}
-        >
-            <Text>{props.title}</Text>
-        </TouchableOpacity>
-    );
-}
-
-const BasicGenres = ['V-Pop', 'K-Pop', 'Pop', 'Blue']
-
-const DefaultPlaylists = () => {
-
-    return (
-        <LinearGradient colors={['#0C08C4', '#030239', '#000000']}>
-
-            <View style={{
-                height: '100%',
-            }}>
-                <View style={{
-                    marginHorizontal: 16,
-
-                }}>
-                    <View style={{
-                        marginTop: 41,
-
-                    }}>
-                        <Text style={{
-                            fontSize: 30,
-                            color:"#ffffff"
-                        }}>Playlist</Text>
-                    </View>
-                    <View style={{
-                        marginTop: 40,
-                    }}>
-                        <Text style={{
-                            fontSize: 17,
-                            color: '#D87777',
-                        }}>Recently played</Text>
-                    </View>
-                    <View>
-                        <FlatList
-                            keyExtractor={(item, index) => index.toString()}
-                            data={SongData.slice(0, 10)}
-                            horizontal={true}
-                            renderItem={({item})=>{
-                                return <Recent
-                                    song={item}
-                                    playlist={SongData.slice(0, 10)}
-                                />
-                        }} />
-                    </View>
-                    <View style={{
-                        display: 'flex',
-                        flexDirection: 'row',
-                        flexWrap: 'wrap'
-                    }}>
-                        <FlatList
-                            keyExtractor={(item, index) => index.toString()}
-                            data={BasicGenres}
-                            renderItem={({item}) => {
-                                return <PlaylistButton title={item} />
-                            }}
-                            numColumns={2}
-                        />
-                    </View>
-                </View>
-            </View>
-        </LinearGradient>
-    );
-}
->>>>>>> 5a0c22cb6bc6bbc483364a1edaa839502fea488b
 
 export default DefaultPlaylists;
